41dd4cf3c45a82a95a7d0a99937fa620aa236a53
Vitalii Lakusta <vitaliy.lakusta@starship.co>
2020-06-08 21:58:32 +0300

    Calculate jacobian to linearize radar measurement

--- src/tools.cpp
+++ src/tools.cpp
@@ -32,5 +32,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+    MatrixXd Hj(3, 4);
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
 
+    auto c1 = px*px + py*py;
+    auto c2 = sqrt(c1);
+    auto c3 = c1 * c2;
+
+    if (fabs(c1) < 0.0001) {
+        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+        return Hj;
+    }
+
+    Hj << (px/c2), (py/c2), 0, 0,
+            -(py/c1), (px/c1), 0, 0,
+            py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+    return Hj;
 }
